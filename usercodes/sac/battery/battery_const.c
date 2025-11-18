1afd79243832aaeee57336724ade6e51dbf2745b
T.Ino <takap.colonel@gmail.com>
2025-11-19 01:37:18 +0900

    LiHvに対応したしきい値に修正の上、コメントを見直し

--- usercodes/sac/battery/battery_const.c
+++ usercodes/sac/battery/battery_const.c
@@ -71,9 +71,11 @@ const float BATTERY_VOLTAGE_LO_ERROR= 4.00f;
 const float BATTERY_VOLTAGE_HI_ERROR= 9.00f;
 
 const BatteryThresholdsTables_t BATTERY_THRESHOLDS_TABLES = {
-/*                  ERROR_L AAA_EMPTY   AAA_WARNING AAA_NORMAL  AAA_FULL    LIPO_EMPTY      LIPO_WARNING    LIPO_NORMAL LIPO_FULL   ERROR_Hi*/
-.rise_thresholds = {4.4,    1.15f*4.0f, 1.20f*4.0f, 1.25f*4.0f, 1.45f*4.0f, 3.50f * 2.0f,   3.60f*2.0f,     3.70f*2.0f, 3.90f*2.0f, 8.5f},
-.fall_thresholds = {4.35,   1.10f*4.0f, 1.15f*4.0f, 1.20f*4.0f, 1.40f*4.0f, 3.40f * 2.0f,   3.50f*2.0f,     3.65f*2.0f, 3.80f*2.0f, 8.4f}
+/*                                                              */
+.rise_thresholds = {/*ERROR_L*/ 4.4,/*AAA_EMPTY*/ 1.15f*4.0f,/*AAA_WARNING*/ 1.20f*4.0f,/*AAA_NORMAL*/ 1.25f*4.0f,/*AAA_FULL*/ \
+                    /*AAA_FULL*/ 1.45f*4.0f,/*LIPO_EMPTY*/ 3.50f * 2.0f,/*LIPO_WARNING*/ 3.60f*2.0f,/*LIPO_NORMAL*/ 3.70f*2.0f,/*LIPO_FULL*/ 4.45f*2.0f,/*ERROR_Hi*/ 10.0f},
+.fall_thresholds = {/*ERROR_L*/ 4.35,/*AAA_EMPTY*/ 1.10f*4.0f,/*AAA_WARNING*/ 1.15f*4.0f,/*AAA_NORMAL*/ 1.20f*4.0f,/*AAA_FULL*/ \
+                    /*AAA_FULL*/ 1.40f*4.0f,/*LIPO_EMPTY*/ 3.40f * 2.0f,/*LIPO_WARNING*/ 3.50f*2.0f,/*LIPO_NORMAL*/ 3.65f*2.0f,/*LIPO_FULL*/ 4.35f*2.0f,/*ERROR_Hi*/ 9.0f}
 };
 
 const uint16_t BATTERY_MASK_10MSCNT  = 100;
